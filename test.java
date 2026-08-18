554fa0c4f89afe0f199f65f31720ea95684566b2
donald <donaldmtundi955@gmail.com>
2026-08-18 11:16:31 +0300

    initial test page

---
+++ test.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing Git commit!");
+        System.out.println("My commit is working successfully.");
+    }
+}
