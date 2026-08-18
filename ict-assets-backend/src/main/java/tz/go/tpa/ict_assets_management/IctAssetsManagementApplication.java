7469234c4ab362eed32c86ec753c7d01c4c01d4f
lucie-tech <luciafisoo@gmail.com>
2026-08-18 10:19:33 +0300

    Add backend application

---
+++ ict-assets-backend/src/main/java/tz/go/tpa/ict_assets_management/IctAssetsManagementApplication.java
@@ -0,0 +1,13 @@
+package tz.go.tpa.ict_assets_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IctAssetsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IctAssetsManagementApplication.class, args);
+	}
+
+}
